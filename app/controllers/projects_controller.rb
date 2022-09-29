329b60be9d6ac1d46a51bfd5c9e5abe8b0d6478c
Nurbol-Xan <nurbolxaydaraliyev007@gmail.com>
2022-09-29 19:08:53 +0500

    base

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -65,6 +65,6 @@ class ProjectsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def project_params
-      params.require(:project).permit(:title, :description, :user_id)
+      params.require(:project).permit(:title, :description, :user_id, images: [])
     end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,5 @@
 class Project < ApplicationRecord
   belongs_to :user
+  has_many_attached :images
+  
 end
